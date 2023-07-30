2510d9009d915025b7d144c693833286a9b71a29
Fran <franciscoerdozain22@gmail.com>
2023-07-30 16:39:20 -0300

    basic footer working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import '../src/components/navbar/navbarStyle.css'
+import '../src/components/footer/footerStyle.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Cart from './pages/cart/Cart';
@@ -8,6 +9,7 @@ import MmNavbar from './components/navbar/Navbar';
 import ProductDetails from './pages/productDetails/ProductDetails';
 import ProductList from './pages/productList/ProductList';
 import Profile from './pages/profile/Profile';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -21,6 +23,7 @@ function App() {
         <Route path="/product/:id" element={ProductDetails} />
         <Route path="/categories/:category/subcategory" element={ProductList} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,37 @@
+import { Button, Col, Container, Nav, Row } from "react-bootstrap";
+import { FaFacebookF, FaYoutube, FaInstagram } from "react-icons/fa";
+
 const Footer = () => {
-    return(
-        <>
-        </>
-    )
+    return (
+        <footer className="bg-dark text-light py-2 fixed-bottom">
+            <Container>
+                <Row>
+                    <Col xs={6} md={6} className="d-flex align-items-center flex-wrap w-100">
+                        <span><strong>Melody Makers</strong> - <em> Giving the Instruments to Unleash Your Musical Potential since 2023</em></span>
+                    </Col>
+                </Row>
+                    <Row>
+                    <Col className="d-flex" >
+                        <Nav className="flex-row d-flex">
+                            <Nav.Link href="https://facebook.com" target="_blank" className="FaFacebookF"><FaFacebookF size={20} /></Nav.Link>
+                            <Nav.Link href="https://youtube.com" target="_blank" className="FaYoutube"><FaYoutube size={25} /></Nav.Link>
+                            <Nav.Link href="https://instagram.com" target="_blank" className="FaInstagram"><img src='/images/logoInstagram.png' className="logoInstagram" /></Nav.Link>
+                        </Nav>
+                    <Col  className="d-flex  align-items-center justify-content-end">
+                        <Button variant="primary" className="me-2">Contact Us</Button>
+                        <Button variant="primary">About Us</Button>
+                    </Col>
+                    </Col>
+                        
+                    </Row>
+
+                <Row>
+                    <Col xs={12} md={6}>
+                        <span>©2023 Melody Makers. All rights reserved - Terms of Service</span>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
 }
 export default Footer;
